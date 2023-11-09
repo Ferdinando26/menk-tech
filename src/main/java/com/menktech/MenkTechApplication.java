0c8b0317eaf89970312b4401cb9af42b2133b897
Lucas Fernando <lucashalves26@gmail.com>
2023-11-09 14:43:26 -0300

    Primeiro passo

---
+++ src/main/java/com/menktech/MenkTechApplication.java
@@ -0,0 +1,13 @@
+package com.menktech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenkTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenkTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/menktech/MenkTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.menktech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenkTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
